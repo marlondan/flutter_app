9e115ac844aedd0620de9acd70d2393bc46afe4e
Marlon Agard <dan@Marlons-Air.fios-router.home>
2019-12-16 15:43:40 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+
+        ),
+        body: Image(image: NetworkImage('https://images.unsplash.com/photo-1574100004472-e536d3b6bacc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')),
+      )
+    ),
+  );
+}
